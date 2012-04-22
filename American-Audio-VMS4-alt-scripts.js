03294d50074d915666004eeccdd20f5dddc77e22
xorik <xorik@notebook>
2012-04-22 11:58:56 +0400

    Toggle flanger by clicking touch slider, flanger depth is depend on the click position

--- American-Audio-VMS4-alt-scripts.js
+++ American-Audio-VMS4-alt-scripts.js
@@ -6,3 +6,13 @@ VMS4.pause = function( channel, control, value, status, group )
 	if( engine.getValue(group, "play") )
 		engine.setValue( group, "play", 0 );
 }
+
+
+// Flanger toggle
+VMS4.flangerToggle = function( channel, control, value, status, group )
+{
+	if( status == 0x90 )
+		engine.setValue( group, "flanger", 1 );
+	else
+		engine.setValue( group, "flanger", 0 );
+}
